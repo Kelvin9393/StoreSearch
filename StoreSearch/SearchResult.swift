9f5659c5f38dfc976c85ab471472078bc4a87f92
Kelvin9393 <kelvinlssiang9393@hotmail.com>
2017-10-22 21:37:36 +0800

    Added search bar and tableView. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by KELVIN LING SHENG SIANG on 21/10/2017.
+//  Copyright © 2017 KELVIN LING SHENG SIANG. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
